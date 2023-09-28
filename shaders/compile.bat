7820bba059b610b390e857b727659a744bc1fc0d
André Neder <andre.neder@mail.de>
2023-09-28 17:56:52 +0200

    remove stuff

--- shaders/compile.bat
+++
@@ -1,3 +0,0 @@
-glslangValidator.exe  -V shader.frag -o compiled/frag.spv --target-env
-glslangValidator.exe -V shader.vert -o compiled/vert.spv --target-env
-pause
--- shaders/compile.sh
+++
@@ -1,2 +0,0 @@
-glslangValidator  -V shader.frag -o compiled/frag.spv --target-env
-glslangValidator -V shader.vert -o compiled/vert.spv --target-env
