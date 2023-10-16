9d07e175c752e10b8162eaa84acb3a639bbc1c56
André Neder <andre.neder@mail.de>
2023-10-16 07:56:48 +0200

    shader stuff

--- src/imgui_renderpass.cpp
+++ src/imgui_renderpass.cpp
@@ -19,7 +19,8 @@ using namespace gpu;
         ImGuiIO& io = ImGui::GetIO(); (void)io;
         //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
         //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
-
+        io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+        // io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
         // Setup Dear ImGui style
         ImGui::StyleColorsDark();
         //ImGui::StyleColorsClassic();
