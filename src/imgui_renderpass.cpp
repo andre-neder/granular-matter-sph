45f024096b9fae12e3365ff7be2e713e34c364fe
André Neder <andre.neder@mail.de>
2023-10-29 17:48:30 +0100

    Dockspace

--- src/imgui_renderpass.cpp
+++ src/imgui_renderpass.cpp
@@ -17,10 +17,12 @@ using namespace gpu;
         IMGUI_CHECKVERSION();
         ImGui::CreateContext();
         ImGuiIO& io = ImGui::GetIO(); (void)io;
+
         //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
         //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
         io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
         // io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
+
         // Setup Dear ImGui style
         ImGui::StyleColorsDark();
         //ImGui::StyleColorsClassic();
@@ -93,39 +95,92 @@ using namespace gpu;
             std::cerr << "Exception Thrown: " << e.what();
         }
     }
-    
+
     void ImguiRenderPass::update(int currentFrame, int imageIndex){
         
         ImGui_ImplVulkan_NewFrame();
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
-        ImGui::Begin("GPU Info", &showGPUInfo);
-        ImGui::Text(m_deviceProperties.deviceName);
-        ImGui::End();
+        static bool p_dockSpaceOpen = true;
+        static bool opt_fullscreen = true;
+        static bool opt_padding = false;
+        static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_PassthruCentralNode;
 
-        ImGui::Begin("Timings", &showGPUInfo);
-        for (size_t i = 0; i < passTimeings.size(); i++) {
-            ImGui::Text(passTimeings[i].c_str());
+        // We are using the ImGuiWindowFlags_NoDocking flag to make the parent window not dockable into,
+        // because it would be confusing to have two docking targets within each others.
+        ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
+        if (opt_fullscreen)
+        {
+            const ImGuiViewport* viewport = ImGui::GetMainViewport();
+            ImGui::SetNextWindowPos(viewport->WorkPos);
+            ImGui::SetNextWindowSize(viewport->WorkSize);
+            ImGui::SetNextWindowViewport(viewport->ID);
+            ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+            ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+            window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
+            window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
         }
-        passTimeings = std::vector<std::string>();
+        else
+        {
+            dockspace_flags &= ~ImGuiDockNodeFlags_PassthruCentralNode;
+        }
+
+        // When using ImGuiDockNodeFlags_PassthruCentralNode, DockSpace() will render our background
+        // and handle the pass-thru hole, so we ask Begin() to not render a background.
+        if (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode)
+            window_flags |= ImGuiWindowFlags_NoBackground;
+
+        // Important: note that we proceed even if Begin() returns false (aka window is collapsed).
+        // This is because we want to keep our DockSpace() active. If a DockSpace() is inactive,
+        // all active windows docked into it will lose their parent and become undocked.
+        // We cannot preserve the docking relationship between an active window and an inactive docking, otherwise
+        // any change of dockspace/settings would lead to windows being stuck in limbo and never being visible.
+        if (!opt_padding)
+            ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+        ImGui::Begin("DockSpace Demo", &p_dockSpaceOpen, window_flags);
+        if (!opt_padding)
+            ImGui::PopStyleVar();
+
+        if (opt_fullscreen)
+            ImGui::PopStyleVar(2);
+
+        // Submit the DockSpace
+        ImGuiIO& io = ImGui::GetIO();
+        if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable)
+        {
+            ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
+            ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
+        }
+
+            ImGui::Begin("GPU Info", &showGPUInfo);
+                ImGui::Text(m_deviceProperties.deviceName);
+            ImGui::End();
+
+            ImGui::Begin("Timings", &showGPUInfo);
+                for (size_t i = 0; i < passTimeings.size(); i++) {
+                    ImGui::Text(passTimeings[i].c_str());
+                }
+                passTimeings = std::vector<std::string>();
+            ImGui::End();
+
+            ImGui::Begin("Simulation", &showSimulationSettings);
+                ImGui::Checkbox("Simulation running", &simulationRunning);
+                ImGui::DragFloat("Rest Density (kg/m^2)", &settings.rho0, 1.f, 0.1f, 2000.f);
+                ImGui::DragFloat("Pressure stiffness", &settings.stiffness, 1.f, 100.f, 50000.f);
+                ImGui::DragFloat("Mass (kg)", &settings.mass, 1.f, 0.1f, 100.f);
+                ImGui::DragFloat("Kernel Radius (m)", &settings.kernelRadius, 1.f, 0.1f, 100.f);
+                ImGui::DragFloat("Angle of repose (rad)", &settings.theta, 1.f, 0.001f, (float)M_PI);
+                ImGui::DragFloat("Viscosity constant", &settings.sigma, 1.f, 0.01f, 10.f);
+                // ImGui::DragFloat("Cohesion intensity", &settings.beta, 1.f, 0.01f, 10.f);
+                // ImGui::DragFloat("Maximum Cohesion", &settings.C, 1.f, 0.01f, 10.f);
+                ImGui::DragFloat2("Gravity (m/s^2)", glm::value_ptr(settings.g));
+            ImGui::End();
+
+            ImGui::ShowDemoWindow(&show_demo_window);
+
         ImGui::End();
 
-        ImGui::Begin("Simulation", &showSimulationSettings);
-        ImGui::Checkbox("Simulation running", &simulationRunning);
-        ImGui::DragFloat("Rest Density (kg/m^2)", &settings.rho0, 1.f, 0.1f, 2000.f);
-        ImGui::DragFloat("Pressure stiffness", &settings.stiffness, 1.f, 100.f, 50000.f);
-        ImGui::DragFloat("Mass (kg)", &settings.mass, 1.f, 0.1f, 100.f);
-        ImGui::DragFloat("Kernel Radius (m)", &settings.kernelRadius, 1.f, 0.1f, 100.f);
-        ImGui::DragFloat("Angle of repose (rad)", &settings.theta, 1.f, 0.001f, (float)M_PI);
-        ImGui::DragFloat("Viscosity constant", &settings.sigma, 1.f, 0.01f, 10.f);
-        // ImGui::DragFloat("Cohesion intensity", &settings.beta, 1.f, 0.01f, 10.f);
-        // ImGui::DragFloat("Maximum Cohesion", &settings.C, 1.f, 0.01f, 10.f);
-        ImGui::DragFloat2("Gravity (m/s^2)", glm::value_ptr(settings.g));
-
-        ImGui::End();
-
-        ImGui::ShowDemoWindow(&show_demo_window);
         ImGui::Render();
 
         vk::CommandBufferBeginInfo beginInfo;
@@ -152,27 +207,19 @@ using namespace gpu;
     }
    
     void ImguiRenderPass::createDescriptorPool() {
-        std::array<vk::DescriptorPoolSize, 11> poolSizes{
-            vk::DescriptorPoolSize{ vk::DescriptorType::eSampler, 1000 },
-            vk::DescriptorPoolSize{ vk::DescriptorType::eCombinedImageSampler, 1000 },
-            vk::DescriptorPoolSize{ vk::DescriptorType::eSampledImage, 1000 },
-            vk::DescriptorPoolSize{ vk::DescriptorType::eStorageImage, 1000 },
-            vk::DescriptorPoolSize{ vk::DescriptorType::eUniformTexelBuffer, 1000 },
-            vk::DescriptorPoolSize{ vk::DescriptorType::eStorageTexelBuffer, 1000 },
-            vk::DescriptorPoolSize{ vk::DescriptorType::eUniformBuffer, 1000 },
-            vk::DescriptorPoolSize{ vk::DescriptorType::eStorageBuffer, 1000 },
-            vk::DescriptorPoolSize{ vk::DescriptorType::eUniformBufferDynamic, 1000 },
-            vk::DescriptorPoolSize{ vk::DescriptorType::eStorageBufferDynamic, 1000 },
-            vk::DescriptorPoolSize{ vk::DescriptorType::eInputAttachment, 1000 }
-        };
-        //vk::DescriptorPoolCreateFlagBits::eFreeDescriptorSet
-        vk::DescriptorPoolCreateInfo descriptorPoolInfo({}, static_cast<uint32_t>(1000 * poolSizes.size()), poolSizes);
-
-        try{
-            descriptorPool = m_core->getDevice().createDescriptorPool(descriptorPoolInfo);
-        }catch(std::exception& e) {
-            std::cerr << "Exception Thrown: " << e.what();
-        }
+        descriptorPool = m_core->createDescriptorPool({
+            { vk::DescriptorType::eSampler, 1000 },
+            { vk::DescriptorType::eCombinedImageSampler, 1000 },
+            { vk::DescriptorType::eSampledImage, 1000 },
+            { vk::DescriptorType::eStorageImage, 1000 },
+            { vk::DescriptorType::eUniformTexelBuffer, 1000 },
+            { vk::DescriptorType::eStorageTexelBuffer, 1000 },
+            { vk::DescriptorType::eUniformBuffer, 1000 },
+            { vk::DescriptorType::eStorageBuffer, 1000 },
+            { vk::DescriptorType::eUniformBufferDynamic, 1000 },
+            { vk::DescriptorType::eStorageBufferDynamic, 1000 },
+            { vk::DescriptorType::eInputAttachment, 1000 }
+        });
     }
     
     void ImguiRenderPass::destroyFrameResources(){
@@ -181,7 +228,7 @@ using namespace gpu;
             device.destroyFramebuffer(framebuffer);
         }
         device.freeCommandBuffers(m_core->getCommandPool(), commandBuffers);
-        m_core->getDevice().destroyDescriptorPool(descriptorPool);
+        m_core->destroyDescriptorPool(descriptorPool);
         m_core->getDevice().destroyRenderPass(renderPass);
     }
     void ImguiRenderPass::destroy(){
--- src/main.cpp
+++ src/main.cpp
@@ -25,8 +25,8 @@
 
 std::vector<std::string> passTimeings = std::vector<std::string>();
 
-const uint32_t WIDTH = 800;
-const uint32_t HEIGHT = 600;
+const uint32_t WIDTH = 1280;
+const uint32_t HEIGHT = 720;
 
 class Application {
 public:
